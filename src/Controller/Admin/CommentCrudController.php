a4be4ac026907263616a1d2649cb64aaffbd2eb1
Sergey Chivirda <si.chivirda@gmail.com>
2023-08-11 22:19:09 +0500

    feat: add view upload photos to admin panel, fix: fix edit comment

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -9,6 +9,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
@@ -35,7 +36,9 @@ class CommentCrudController extends AbstractCrudController
         yield EmailField::new('email');
         yield TextareaField::new('text')
             ->hideOnIndex();
-        yield TextField::new('photoFileName')
+        yield ImageField::new('photoFileName')
+            ->setBasePath('/uploads/photos')
+            ->setLabel('Photo')
             ->onlyOnIndex();
         $createdAt = DateTimeField::new('createdAt')->setFormTypeOptions(
             [
@@ -44,7 +47,7 @@ class CommentCrudController extends AbstractCrudController
             ]
         );
         if (Crud::PAGE_EDIT === $pageName) {
-            yield $createdAt->setFormTypeOptions('disabled', true);
+            yield $createdAt->setFormTypeOptions(['disabled' => true]);
         }
     }
 }
